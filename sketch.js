1def7b4475101e0cc8969974f6149cf4311c80fc
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2016-10-01 21:20:16 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+/*
+Boilerplate for ae_to_p5js
+
+export keyframes from ae using script exportPositionKeyframes (from this repo)
+put the exported txt file in the data folder of this sketch
+*/
+
+var ae_rawKeys, ae_keys, ae_x, ae_y ;
+
+
+function preload() {
+    ae_rawKeys = loadStrings('data/keyframes.txt');
+}
+
+
+function parseKeyframes(k) {
+    var parsedKeys = [];
+    var rawKeys = k[0].split('*');
+    for (var i = 0; i < rawKeys.length - 1; i++) {
+        var pos = rawKeys[i].match(/\d+/g);
+        var posx = float(pos[0]);
+        var posy = float(pos[1]);
+        parsedKeys.push({x: posx, y: posy});
+    }
+    return parsedKeys;
+}
+
+
+function ae_getPos() {
+    ae_x = ae_keys[frameCount % ae_keys.length].x;
+    ae_y = ae_keys[frameCount % ae_keys.length].y;
+}
+
+
+function setup() {
+    createCanvas(1280, 720);
+    ae_keys = parseKeyframes(ae_rawKeys);
+    frameRate(30);
+}
+
+
+function draw() {
+    background(0);
+    fill(255);
+    noStroke();
+    ae_getPos();
+    ellipse(ae_x, ae_y, 50, 25);
+}
