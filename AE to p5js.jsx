47bea4b85abbcf9e508f61242a72819a68741cc4
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2016-10-02 18:56:32 +0200

    Add files via upload

---
+++ AE to p5js.jsx
@@ -0,0 +1,41 @@
+/*
+Export position keyframes of the selected layer
+After Effects 2015.3
+
+usage:
+Select a layer with position keyframes and run this script.
+Import the exported .txt file in a p5js file using the p5js sketch in this repo
+*/
+
+
+// get the position keyframes of the selected layer
+function getKeyframes() {
+    postxt = '';
+    var comp = app.project.activeItem;
+    var lyr = comp.selectedLayers[0];
+    var maxFrames = comp.duration * 30;
+
+    for (var frame = 0; frame < maxFrames; frame++) {
+        var pos = lyr.transform.position.valueAtTime(comp.frameDuration * frame, true);
+        posx = pos[0];
+        posy = pos[1];
+        postxt += Math.round(posx) + ',' + Math.round(posy) + '\n';
+    }
+
+    return postxt;
+}
+
+
+// save the keyframes to a .txt file
+function saveKeyframes(c) {
+    c = c.join(',');
+    var doc = new File('~/Desktop/keyframes.txt');
+    doc.open("w");
+    doc.write(c);
+    doc.close();
+}
+
+
+var keys = getKeyframes();
+saveKeyframes(keys);
+alert('Keyframes exported!');
